ff075dd3f1088ecfb32d9fbf1991e27106377789
thresh <17614509521@163.com>
2023-07-29 00:25:05 +0800

    feat: 基于文件的路由系统

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <!-- 路由链接 -->
+  <NuxtLink to="/">首页</NuxtLink>
+  <NuxtLink to="/video">视频页</NuxtLink>
+  <!-- 页面路由 -->
+  <NuxtPage />
+</template>
+<script lang="ts" setup></script>
